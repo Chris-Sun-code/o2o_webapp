ac8c5d0d15fec35a7bd8b9c459cf5f099bf90778
Chris <sunhaonan_888.@163.com>
2019-11-25 12:32:31 +0800

    定义商品操作异常类

---
+++ src/main/java/com/haonan/o2o/exceptions/ProductOperationException.java
@@ -0,0 +1,14 @@
+package com.haonan.o2o.exceptions;
+
+public class
+ProductOperationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5076172298827469013L;
+
+	public ProductOperationException(String msg) {
+		super(msg);
+	}
+}
