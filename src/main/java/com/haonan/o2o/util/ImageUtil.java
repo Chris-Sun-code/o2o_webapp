b3c209d4e5af91d92edfb8f7725b36c35de4274c
Chris <sunhaonan_888.@163.com>
2019-11-26 20:14:45 +0800

    新增商品详情处理方法

--- src/main/java/com/haonan/o2o/util/ImageUtil.java
+++ src/main/java/com/haonan/o2o/util/ImageUtil.java
@@ -81,6 +81,40 @@ public class ImageUtil {
         return relativeAddr;
     }
 
+    /**
+     * 处理详情图， 并返回新生成图片的相对路径
+     *
+     * @param thumbnail
+     * @param targetAddr
+     * @return
+     */
+    public static String generateNormalImg(ImageHolder thumbnail, String targetAddr) {
+        // 获取不重复的随机名
+        String realFileName = getRandomFileName();
+        // 获取文件的扩展名如png,jpg等
+        String extension = getFileExtension(thumbnail.getImageName());
+        // 如果目标路径不存在，则自动创建
+        makeDirPath(targetAddr);
+        // 获取文件存储的相对路径(带文件名)
+        String relativeAddr = targetAddr + realFileName + extension;
+        logger.debug("current relativeAddr is :" + relativeAddr);
+        // 获取文件要保存到的目标路径
+        File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+        logger.debug("current complete addr is :" + PathUtil.getImgBasePath() + relativeAddr);
+        logger.debug("basePath is :" + basePath);
+        // 调用Thumbnails生成带有水印的图片
+        try {
+            Thumbnails.of(thumbnail.getImage()).size(337, 640)
+                    .watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "/watermark.jpg")), 0.25f)
+                    .outputQuality(0.9f).toFile(dest);
+        } catch (IOException e) {
+            logger.error(e.toString());
+            throw new RuntimeException("创建缩略图失败：" + e.toString());
+        }
+        // 返回图片相对路径地址
+        return relativeAddr;
+    }
+
 
     /**
      * 生成随机文件名，当前年月日小时分钟秒钟+五位随机数
