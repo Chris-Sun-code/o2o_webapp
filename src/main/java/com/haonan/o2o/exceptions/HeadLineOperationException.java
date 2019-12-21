19b07b96a9a3d1fb9395386fe093e06f0e477814
Chris <sunhaonan_888.@163.com>
2019-12-21 19:07:37 +0800

    前台首页头条异常

---
+++ src/main/java/com/haonan/o2o/exceptions/HeadLineOperationException.java
@@ -0,0 +1,13 @@
+package com.haonan.o2o.exceptions;
+
+public class HeadLineOperationException extends RuntimeException {
+
+	/**
+	 * 前台首页头条异常
+	 */
+	private static final long serialVersionUID = -6866853564850051110L;
+
+	public HeadLineOperationException(String msg) {
+		super(msg);
+	}
+}
