796b38ae7d63c073d00746c582fdd5a1400e52ef
Chris <sunhaonan_888.@163.com>
2019-12-25 11:46:09 +0800

    店铺区域操作异常

---
+++ src/main/java/com/haonan/o2o/exceptions/AreaOperationException.java
@@ -0,0 +1,13 @@
+package com.haonan.o2o.exceptions;
+
+public class AreaOperationException extends RuntimeException {
+
+	/**
+	 * 店铺区域异常类
+	 */
+	private static final long serialVersionUID = -1512771573934050550L;
+
+	public AreaOperationException(String msg) {
+		super(msg);
+	}
+}
